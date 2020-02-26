8e6067882d464ab565d6735601cf15ba0c2c4a3c
Christian Martinez <christianbm1@gmail.com>
2020-02-26 21:48:40 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(val)
-  puts(val)
-  return val
+def parrot()
+  term = "Squawk!"
+  puts(term)
+  return term
 end
